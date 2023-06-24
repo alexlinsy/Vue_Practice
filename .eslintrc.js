55c16a9e2715afd5642498463ddd9ed0b21c6a58
shaoyi.lin <shaoyi.lin@avanade.com>
2023-06-23 17:08:18 -0700

    Enabling inter-component communication

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,9 +9,12 @@ module.exports = {
   ],
   parserOptions: {
     parser: '@babel/eslint-parser',
+    requireConfigFile: false,
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'linebreak-style': 0,
+    'vue/no-multiple-template-root': 'off',
   },
 };
---
+++ src/shared/CollapsibleSection.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <div class="header">
+            <span
+              v-if="open"
+              @click="open = !open"
+              @keydown.enter="open = !open">
+              &#x25B2; Collapse
+            </span>
+            <span
+              v-if="!open"
+              @click="open = !open"
+              @keydown.enter="open = !open">
+              &#x25BC; Expand
+            </span>
+        </div>
+        <slot v-if="open">
+        </slot>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'CollapsibleSection',
+  data() {
+    return {
+      open: false,
+    };
+  },
+};
+</script>
+
+<style scoped>
+ .header {
+    background-color: grey;
+    padding: 3px;
+    cursor: pointer;
+ }
+</style>
